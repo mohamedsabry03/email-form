2fe189cc84351f84aa14005a9f88861b414da504
Alexandra Nehme <alexandramarie03n@gmail.com>
2024-05-14 17:45:36 -0500

    commiting flask_app changes

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,19 @@ import os
 
 app = Flask(__name__, template_folder='templates')
 
-emails_df = pd.DataFrame(columns=['emails'])
+
+def load_from_csv():
+    csv_filename = 'emails.csv'
+    csv_path = os.path.join(os.path.expanduser("~"), 'mysite', csv_filename)
+    if os.path.exists(csv_path):
+        return pd.read_csv(csv_path)
+    else:
+        return pd.DataFrame(columns=['emails'])
+
+def save_to_csv(df):
+    csv_filename = 'emails.csv'
+    csv_path = os.path.join(os.path.expanduser("~"), 'mysite', csv_filename)
+    df.to_csv(csv_path, index=False)
 
 @app.route('/')
 def index():
@@ -12,18 +24,20 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit_form():
-    global emails_df
+    emails_df = load_from_csv()
     email = request.form['email']
     if not email.endswith('@uchicago.edu'):
         return redirect('/failure')
-    else:
-        emails_df = emails_df.append({'emails': email}, ignore_index=True)
-        if emails_df['emails'].str.lower().duplicated().any():
-            return redirect('/duplicate')
-        else:
-            emails_df = emails_df.sample(frac=1).reset_index(drop=True)
-            save_to_csv()
-            return redirect('/success')
+
+    new_email = email.lower()
+    if new_email in emails_df['emails'].str.lower().values:
+        return redirect('/duplicate')
+
+    emails_df = emails_df.append({'emails': email}, ignore_index=True)
+    emails_df = emails_df.sample(frac=1).reset_index(drop=True)
+    save_to_csv(emails_df)  # Pass emails_df as an argument
+    return redirect('/success')
+
 
 @app.route('/success')
 def success():
@@ -36,12 +50,6 @@ def failure():
 @app.route('/duplicate')
 def duplicate():
     return render_template('duplicate.html')
-
-def save_to_csv():
-    global emails_df
-    csv_filename = 'emails.csv'
-    csv_path = os.path.join(os.path.expanduser("~"), 'mysite', csv_filename)
-    emails_df.to_csv(csv_path, index=False)
 
 if __name__ == '__main__':
     app.run(debug=True)
